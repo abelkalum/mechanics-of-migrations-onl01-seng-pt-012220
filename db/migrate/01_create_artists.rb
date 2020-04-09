7d879bbb3d308a72a3b8ad05cff5d18496873b04
Abel Kalum <chumbabel@gmail.com>
2020-04-09 15:46:56 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+    create_table :artists do |x|
+			x.string :name
   end
 end
